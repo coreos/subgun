07e67d964a20c01ac525730f2dc33fb83a026e50
Brandon Philips <brandon@ifup.co>
2013-10-30 06:23:03 -0700

    feat(subgun): handle unsubscribe too

--- subgun.go
+++ subgun.go
@@ -8,6 +8,9 @@ import (
 	"net/http"
 	"os"
 	"time"
+	"net/url"
+	"path"
+	"strings"
 
 	"github.com/gorilla/mux"
 	"github.com/philips/go-mailgun"
@@ -73,9 +76,9 @@ func listAllowed(list string) bool {
 	return false
 }
 
-// subscribeConfirmHandler handles a confirmation link and changes the persons
-// subscription state to "subscribed" if the token matches.
-func subscribeConfirmHandler(w http.ResponseWriter, r *http.Request) {
+// confirmationHandler handles a confirmation link and changes the persons
+// subscription state to "subscribed" or "unsubscribed" if the token matches.
+func confirmationHandler(w http.ResponseWriter, r *http.Request) {
 	muxVars := mux.Vars(r)
 
 	listName := muxVars["list"]
@@ -92,7 +95,7 @@ func subscribeConfirmHandler(w http.ResponseWriter, r *http.Request) {
 
 	token := muxVars["token"]
 	if len(token) == 0 {
-		http.Error(w, "No subscription token!", 400)
+		http.Error(w, "No token!", 400)
 		return
 	}
 
@@ -103,29 +106,36 @@ func subscribeConfirmHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Success! Finish up the subscription.
-	if token != member.Vars["SubscribeToken"] {
+	action := muxVars["action"]
+	if token != member.Vars[strings.Title(action) + "Token"] {
 		http.Error(w, "Bad confirmation token", 400)
 		return
 	}
 
-	member.Subscribed = true
+	if action == "subscribe" {
+		member.Subscribed = true
+		fmt.Fprintf(w, "Success! You are now subscribed to %s", listName)
+	} else if action == "unsubscribe" {
+		member.Subscribed = false
+		fmt.Fprintf(w, "Success! You are now unsubscribed from %s", listName)
+	} else {
+		http.Error(w, fmt.Sprintf("Unknown action %s", action), 500);
+		return
+	}
+
 	_, err = mg.UpdateListMember(listName, member)
 	if err != nil {
 		http.Error(w, "Internal error", 500)
 		fmt.Println(err)
 		return
 	}
-
-	fmt.Fprintf(w, "Success! You are now subscribed to %s", listName)
-
 	return
 }
 
-// subscribeHandler adds the requested email to the list as unsubscribed and stores
-// a Subscribe token. The token is sent to the email address to ensure the owner
-// actually subscribed.
-func subscribeHandler(w http.ResponseWriter, r *http.Request) {
+// initialHandler subscribes or unsubscribes the requested email from the list
+// and sends a confirmation email with a token to ensure the email owner actually
+// requested the action.
+func initialHandler(w http.ResponseWriter, r *http.Request) {
 	muxVars := mux.Vars(r)
 
 	// Deal with CORS stuff
@@ -143,34 +153,95 @@ func subscribeHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if listAllowed(listName) == false {
-		// TODO: check the right return code for not-allowed
-		http.Error(w, "Unknown list.", 404)
+		http.Error(w, "Unknown list.", 403)
 		return
 	}
 
 	email := r.FormValue("email")
 
+	action := muxVars["action"];
+
+	switch action {
+	case "subscribe": handleSubscribe(w, listName, email);
+	case "unsubscribe": handleUnsubscribe(w, listName, email);
+	default: http.Error(w, fmt.Sprintf("Unknown action %s", action), 500);
+	}
+}
+
+// confirmURL generates a url.URL for a confirmation link that the user will
+// get via and must click to confirm a request.
+func confirmURL(action string, listName string, email string, key string) url.URL {
+	u := url.URL{}
+	u.Scheme = "http"
+	u.Host = cfg.Subscribegun.Hostname
+	u.Path = path.Join("/", action, listName, "confirm", email, key)
+	return u
+}
+
+func addListMember(listName string, email string) (string, error) {
 	// Generate the tokens for the user
 	vars := map[string]string{
 		"UnsubscribeToken": randomString(16),
 		"SubscribeToken":   randomString(16),
 	}
 	member := mailgun.ListMember{email, false, vars, "", ""}
-
 	key := vars["SubscribeToken"]
 
 	_, err := mg.AddListMember(listName, member)
+	return key, err
+}
+
+// handleSubscribe handles a subscribe request.
+func handleSubscribe(w http.ResponseWriter, listName string, email string) {
+	var key string
+
+	member, err := mg.GetListMember(listName, email)
+	// Try to add the member if it doesn't exist
+	if err != nil {
+		key, err = addListMember(listName, email)
+	} else {
+		key = member.Vars["SubscribeToken"]
+	}
+	
+
 	if err != nil {
 		http.Error(w, "Internal error", 500)
 		fmt.Println(err)
 		return
 	}
 
+	u := confirmURL("subscribe", listName, email, key)
 	confirmMail := mail{
 		from:    "no-reply@lists.coreos.com",
 		to:      []string{email},
 		subject: "confirm subscription to " + listName,
-		text:    "click here to confirm http://" + cfg.Subscribegun.Hostname + "/subscribe/" + listName + "/confirm/" + email + "/" + key,
+		text:    "click here to confirm your subscription request to " + listName + ":\n" + u.String(),
+	}
+	_, err = mg.Send(&confirmMail)
+
+	if err != nil {
+		http.Error(w, "Internal error", 500)
+		fmt.Println(err)
+		return
+	}
+}
+
+// handleUnsubscribe handles an unsubscribe request.
+func handleUnsubscribe(w http.ResponseWriter, listName string, email string) {
+	member, err := mg.GetListMember(listName, email)
+	if err != nil {
+		http.Error(w, "Internal error", 500)
+		fmt.Println(err)
+		return
+	}
+	key := member.Vars["UnsubscribeToken"]
+
+	u := confirmURL("unsubscribe", listName, email, key)
+	confirmMail := mail{
+		from:    "no-reply@lists.coreos.com",
+		to:      []string{email},
+		subject: "confirm unsubscribe to " + listName,
+		text:    "click here to confirm your unsubscribe request to " + listName + ":\n" + u.String(),
 	}
 	_, err = mg.Send(&confirmMail)
 
@@ -195,18 +266,13 @@ func main() {
 
 	mg = mailgun.New(cfg.Mailgun.Key)
 
-	r := mux.NewRouter()
-
 	// TODO: add a secret seed in here
 	rand.Seed(time.Now().UTC().UnixNano())
 
-	// subscription handling
-	r.HandleFunc("/subscribe/{list}", subscribeHandler)
-	r.HandleFunc("/subscribe/{list}/confirm/{email}/{token}", subscribeConfirmHandler)
+	r := mux.NewRouter()
 
-	// TODO: unsubscribe handling
-	// http.HandleFunc("/unsubscribe", handler)
-	// http.HandleFunc("/unsubscribe/confirm", handler)
+	r.HandleFunc("/{action}/{list}", initialHandler)
+	r.HandleFunc("/{action}/{list}/confirm/{email}/{token}", confirmationHandler)
 
 	http.ListenAndServe(":8080", r)
 }
