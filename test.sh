9dc9923ab41df30b982eb3d4dd95f253fd2b5940
Brian Waldon <bcwaldon@gmail.com>
2014-02-01 08:12:07 -0800

    chore(test): Add test-runner

---
+++ test.sh
@@ -0,0 +1,18 @@
+#!/bin/sh -e
+
+if [ -z "$PKG" ]; then
+    PKG=""
+fi
+
+# Get GOPATH, etc from build
+. ./build
+
+# use right GOPATH
+export GOPATH="${PWD}"
+
+# Unit tests
+for i in $PKG
+do
+    go test -i $i
+    go test -v $i
+done
